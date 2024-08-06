16f984ea726bf33ad94b6b256a01457270aaf06f
Deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-08-06 18:53:49 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+function sum(a,b,c=3){
+    //console.log(a+b);
+    return a + b + c
+
+}
+
+result = sum(2,2);
+result = sum(4,2);
+result = sum(1,8,9);
+
+console.log("the sum of this 2 numbers is :",result);
+
+const func = (x)=>{
+    console.log(" i am an arrow function",x);
+
+
+}
+
+func(32);
