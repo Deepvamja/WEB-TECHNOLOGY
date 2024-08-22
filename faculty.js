03e3e44dd4df89dd850c5a3ac30fff4a72d73bb0
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-08-22 09:00:20 +0530

    Add files via upload

--- faculty.js
+++ faculty.js
@@ -1,18 +1,59 @@
-
+const express = require ('express');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const faculty = require("./demo.js");
+const Faculty = require("./demo.js");
 
 
-mongoose
-        .connect("mongodb+srv://DeepVamja:Deepvamja2211@cluster0.n07en.mongodb.net/")
+mongoose.connect("mongodb+srv://DeepVamja:deepvamja2211@cluster0.n07en.mongodb.net/")
         .then(()=>{
-            const express = require ("express");
+            console.log("commected with cloud database");
+
+            //const express = require ("express");
             const app = express();
             app.use(bodyParser.urlencoded({extended:false}));
 
-            
+//get all
+
+app.get('/Facultys',async(req,res)=>{
+    const ans = await Faculty.find();
+    res.send(ans);
+
+});
+
+//get by id
+
+app.get('/Facultys/:id',async(req,res)=>{
+    const ans = await Faculty.findone({id:req.params.id});
+    res.send(ans);
+    
+});
+
+//create
+app.post('/Facultys',async(req,res)=>{
+    Fac = new Faculty({...req.body})
+    const ans = await Fac.save();
+    res.send(ans);
+    
+});
+
+//delete
+app.delete('/Facultys/:id',async(req,res)=>{
+    const ans = await Faculty.deleteOne({id:req.params.id});
+    res.send(ans);
+    
+});
+
+//update
+app.patch('/Facultys/:id',async(req,res)=>{
+    const Fac = await Faculty.findOne({id:req.params.id});
+    Fac.id=req.body.id;
+    Fac.name=req.body.name;
+    const ans = await Fac.save()
+    res.send(ans);
+    
+})
+    
         app.listen(2000,function(){
             console.log("server started at 2000 port");
         })
-        })
+ })
