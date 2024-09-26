8f69e9b0f06d21c192f25a43c02289eeb210be62
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-09-26 07:55:48 +0530

    Add files via upload

---
+++ PropsDemo.js
@@ -0,0 +1,10 @@
+function PropsDemo(props){
+    return(
+        <div>
+            <h2>Hello {props.user}</h2>
+            <h3>Welcome to props in react js </h3>
+         </div>
+    );
+}
+
+export default PropsDemo
