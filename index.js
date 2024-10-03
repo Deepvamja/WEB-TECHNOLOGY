849ff32543a1434336b837ada8e2d911b802a30c
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-10-03 12:00:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+const http = require('http');
+const port = 5000;
 
+const server = http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('content-type','text/html');
+    res.end('hello world \n');
 
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+});
 
-
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Hooks from './pages/hooks';
-
-
-// import './index.css';
-// import App from './App';
-// import Comp from './Comp';
-// import PropsDemo from './PropsDemo';
-// import Events from './Events';
-
-
-
-
-// functional components
-
-// function Display(){
-//   return <h3>hello world from react</h3>
-// }
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-  <>
-  <BrowserRouter>
-
-  <Routes>
-    <Route path='/' element={<Layout/>} >
-    
-    <Route index element ={<Home/>}></Route>
-    <Route path="about" element ={<About/>}></Route>
-    <Route path="contact" element ={<Contact/>}></Route>
-    <Route path="hooks" element={<Hooks/>}></Route>
-
-
-    </Route>
-  </Routes>
-  </BrowserRouter>
-
-  
-    </>
-);
-
-
-
-
-
-
-
+server.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}/`);
+});
 
