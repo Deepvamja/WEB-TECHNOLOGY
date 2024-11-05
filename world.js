4be56d000a3ecd6ad91941c014f6e3fda42458c8
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-11-05 09:15:32 +0530

    Update

---
+++ world.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        HTML defer Attribute
+    </title>
+</head>
+
+<body style="text-align:center;">
+    <h1 style="color:green"> 
+        GeeksForGeeks 
+    </h1>
+
+    <h2> 
+        HTML defer Attribute
+    </h2>
+
+    <script id="myGeeks"
+            type="text/javascript"
+            src="my_script.js"
+            defer>
+    </script>
+    <br>
+    <button type="button"
+            onclick="myFunction()">
+      Submit
+  </button>
+
+</body>
+
+</html>
