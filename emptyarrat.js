b668ab5b8b7b02c8208d306b2898a104728d9018
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-11-07 22:05:25 +0530

    Create emptyarray.js

---
+++ emptyarrat.js
@@ -0,0 +1,12 @@
+let Arr = [1, 2, 3, 4, 5];
+
+console.log("Array Elements: " + Arr);
+console.log("Array Length: " + Arr.length);
+
+function emptyArray() {
+    Arr = [];
+    console.log("Empty Array Elements: " + Arr);
+    console.log("Empty Array Length: " + Arr.length);
+}
+
+emptyArray();
