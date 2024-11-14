00c908becbab448801ed9847b4466cea971f8128
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-11-14 09:26:24 +0530

    Update

---
+++ dynamicobject.js
@@ -0,0 +1,13 @@
+const dynamicKey1 = 'age';
+const dynamicValue1 = 19;
+
+const dynamicKey2 = 'city';
+const dynamicValue2 = 'rajkot';
+
+const dynamicObject = {
+    name: 'deep',
+    [dynamicKey1]: dynamicValue1,
+    [dynamicKey2]: dynamicValue2,
+};
+
+console.log(dynamicObject);
